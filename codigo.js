82ba3a6eae35637bac1a8fd634a6293f41e8e7e7
Mateo Cao <mateocao12@gmail.com>
2023-01-25 23:24:08 -0300

    Version final

---
+++ codigo.js
@@ -0,0 +1,80 @@
+//ScrollReveal
+
+window.sr = ScrollReveal();
+
+    sr.reveal(".header__name-container", {
+        delay: 200,
+        duration: 3000,
+        origin: "bottom",
+        distance: "-50px" 
+    });
+
+    sr.reveal(".about-me__text-container", {
+        delay: 200,
+        duration: 3500,
+        origin: "right",
+        distance: "-20px"
+    });
+
+    sr.reveal(".about-me__info-container", {
+        delay: 200,
+        duration: 4000,
+        origin: "bottom",
+        distance: "-20px"
+    });
+
+    sr.reveal(".title-scroll", {
+        delay: 200,
+        duration: 3000,
+        origin: "right",
+        distance: "-50px"
+    });
+
+    sr.reveal(".title-line-scroll", {
+        delay: 200,
+        duration: 3000,
+        origin: "right",
+        distance: "-50px"
+    });
+
+    sr.reveal(".studies__info-container", {
+        delay: 1000,
+        duration: 3000,
+        distance: "50px",
+        origin: "bottom"
+    });
+
+    sr.reveal(".my-experience__info-container", {
+        delay: 1000,
+        duration: 4000,
+        distance: "50px",
+        origin: "left"
+    });
+
+    sr.reveal(".fa-facebook", {
+        delay: 1200,
+        duration: 1500,
+        distance: "50px",
+        origin: "bottom"
+    });
+
+    sr.reveal(".fa-twitter", {
+        delay: 1400,
+        duration: 1500,
+        distance: "50px",
+        origin: "bottom"
+    });
+
+    sr.reveal(".fa-instagram", {
+        delay: 1600,
+        duration: 1500,
+        distance: "50px",
+        origin: "bottom"
+    });
+
+    sr.reveal(".fa-linkedin", {
+        delay: 1800,
+        duration: 1500,
+        distance: "50px",
+        origin: "bottom"
+    });
